2957bf8b350ec01cda2c441f93220fe5228b6fac
Can Gurkan <cangurkan@me.com>
2022-10-12 00:55:09 -0500

    Added tower sim.

--- generate.py
+++ generate.py
@@ -1,10 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
 length = 1; width = 1; height = 1
-x = 0; y = 0; z = height
+x = 0; y = 0; z = height/2.
 
-pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x,y,z/2.] , size=[width,length,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+width,y,z/2.+height] , size=[width,length,height])
+pyrosim.Start_SDF("boxes_tower.sdf")
+for i in range(10):
+    if i != 0:
+        z = z + height
+        length *= .9; width *= .9; height *= .9
+    pyrosim.Send_Cube(name="Box"+str(i), pos=[x, y, z], size=[width, length, height])
 pyrosim.End()
 
