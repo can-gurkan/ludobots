252a1901d8e0fd8fca71527f79b3dc191ba0302e
Can Gurkan <cangurkan@me.com>
2022-10-12 00:36:25 -0500

    Added multiple links.

--- generate.py
+++ generate.py
@@ -1,6 +1,10 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1; width = 1; height = 1
+x = 0; y = 0; z = height
+
+pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x,y,z/2.] , size=[width,length,height])
+pyrosim.Send_Cube(name="Box2", pos=[x+width,y,z/2.+height] , size=[width,length,height])
 pyrosim.End()
 
