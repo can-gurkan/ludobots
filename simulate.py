13df340c116cdf38ec854a133bf25ab0e3dfb660
Can Gurkan <cangurkan@me.com>
2022-10-12 23:31:54 -0500

    Added leg touch sensors and analysis script.

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import pybullet_data
 import time
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -8,8 +10,19 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
 for i in range(1000):
     p.stepSimulation()
-    time.sleep(1./6000)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    time.sleep(1./500)
     print(i)
 p.disconnect()
+
+numpy.save('data/BackLegSensorData.npy',backLegSensorValues)
+numpy.save('data/FrontLegSensorData.npy',frontLegSensorValues)
+print(backLegSensorValues)
+
