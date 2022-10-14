215a3eb764044e6384f3fb46cd64b308136e512c
Can Gurkan <cangurkan@me.com>
2022-10-14 18:59:50 -0500

    Added random motor movement.

--- generate.py
+++ generate.py
@@ -12,10 +12,10 @@ def Create_Robot():
     x = 0; y = 0; z = height / 2.
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x, y, 1.5], size=[width, length, height])
-    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0, 0.5, 1])
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0, 0.5, -0.5], size=[width, length, height])
-    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0, -0.5, 1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[0, -0.5, -0.5], size=[width, length, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[width, length, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[width, length, height])
     pyrosim.End()
 
 
--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -18,11 +19,13 @@ for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-numpy.pi/2,numpy.pi/2), maxForce=50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-numpy.pi/2,numpy.pi/2), maxForce=50)
     time.sleep(1./500)
     print(i)
 p.disconnect()
 
 numpy.save('data/BackLegSensorData.npy',backLegSensorValues)
 numpy.save('data/FrontLegSensorData.npy',frontLegSensorValues)
-print(backLegSensorValues)
+#print(backLegSensorValues)
 
