f79d2b569b4e6be22ed85a59e914e3a59bb2beab
Can Gurkan <cangurkan@me.com>
2022-10-14 20:07:04 -0500

    Added open-loop control to motors.

--- simulate.py
+++ simulate.py
@@ -15,12 +15,18 @@ pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = numpy.zeros(1000)
 frontLegSensorValues = numpy.zeros(1000)
+amplitudeBL = numpy.pi/4; frequencyBL = 20;  phaseOffsetBL = 0
+amplitudeFL = numpy.pi/4; frequencyFL = 20;  phaseOffsetFL = numpy.pi/4
+theta = numpy.linspace(0, 2*numpy.pi, 1000)
+targetAnglesBL = amplitudeBL * numpy.sin(theta * frequencyBL + phaseOffsetBL)
+targetAnglesFL = amplitudeFL * numpy.sin(theta * frequencyFL + phaseOffsetFL)
+
 for i in range(1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-numpy.pi/2,numpy.pi/2), maxForce=50)
-    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=random.uniform(-numpy.pi/2,numpy.pi/2), maxForce=50)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_BackLeg', controlMode=p.POSITION_CONTROL, targetPosition=targetAnglesBL[i], maxForce=15)
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName='Torso_FrontLeg', controlMode=p.POSITION_CONTROL, targetPosition=targetAnglesFL[i], maxForce=15)
     time.sleep(1./500)
     print(i)
 p.disconnect()
